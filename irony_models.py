41a6ad2512a907b005f903bc51ba0662204e33c1
Glutek <glutonator@gmail.com>
2019-06-01 23:44:51 +0200

    first model

--- irony_models.py
+++ irony_models.py
@@ -13,9 +13,11 @@ from load_files import load_vectors
 from preprocesing import print_all
 
 import keras
+import matplotlib.pyplot as plt
 
 # data: DataFrame = load_vectors('vector_test_20.txt')
-data: DataFrame = load_vectors('vector_test_100.txt')
+# data: DataFrame = load_vectors('vector_test_100.txt')
+data: DataFrame = load_vectors('vector_test_full_good_01_06_2019.txt')
 print(data.dtypes)
 print("___________________")
 
@@ -48,7 +50,6 @@ print("xxxxx")
 number_of_sentences = len(dataX_numpy)
 print(number_of_sentences)
 
-
 max_sentence_length = 15
 list_of_sentences = []
 for sentence_index in range(0, number_of_sentences):
@@ -56,14 +57,18 @@ for sentence_index in range(0, number_of_sentences):
 
     if len(sentence) < max_sentence_length:
         while len(sentence) < max_sentence_length:
+            # print(sentence)
+            # print(len(sentence))
+            if len(sentence) == 0:
+                sentence = np.array([np.zeros(25)])
+                # print(sentence)
             zeros = np.array([np.zeros(25)])
-            sentence = np.concatenate((sentence , zeros))
+            sentence = np.concatenate((sentence, zeros))
 
         list_of_sentences.append(sentence)
     else:
         sentence = sentence[0:max_sentence_length]
         list_of_sentences.append(sentence)
-
 
 # print(list_of_sentences)
 dataX_numpy = np.asarray(list_of_sentences)
@@ -72,19 +77,15 @@ print(dataX_numpy.shape)
 # arrayX.reshape((20, 20, 25))
 print(dataX_numpy.shape)
 
-
-
 print("___________________")
 ############
 ##YYYYYYYY
 dataY_numpy = numpy.array(dataY['Label'].values)
 print(dataY_numpy.shape)
 
-
-
-#25 długoś wektora embediningow
-#11 dlugość zdania (licba wektoróœ
-#1 liczba zdan
+# 25 długoś wektora embediningow
+# 11 dlugość zdania (licba wektoróœ
+# 1 liczba zdan
 
 len_of_vector_embeddings = 25
 model = Sequential()
@@ -100,14 +101,29 @@ model.add(Dense(1, activation='sigmoid'))
 # model.add(Dense(2, activation='softmax'))
 model.summary()
 
-model.compile(loss='binary_crossentropy', optimizer='adam')
+model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
 #
 
+save_best = keras.callbacks.ModelCheckpoint('best.weights', monitor='val_loss', verbose=1, save_best_only=True)
+early_stop = keras.callbacks.EarlyStopping(monitor='val_loss', min_delta=0, patience=10, verbose=1)
+
+results = model.fit(dataX_numpy, dataY_numpy, validation_split=0.2,
+                    callbacks=[early_stop], epochs=10, batch_size=10,
+                    verbose=1)
 
 
-model.fit(dataX_numpy, dataY_numpy, epochs=2, batch_size=1, verbose=2)
+saveToFile = "robocza_nazwa_loss"
+# plt.figure(figsize=[8,6])
+plt.plot(results.history['loss'], 'r', linewidth=3.0)
+plt.plot(results.history['val_loss'], 'b', linewidth=3.0)
+plt.legend(['Training loss', 'Val Loss'], fontsize=18)
+plt.xlabel('Number of epochs ', fontsize=16)
+plt.ylabel('Loss', fontsize=16)
+plt.title('Loss Plot', fontsize=16)
+plt.show()
+# plt.savefig(saveToFile)
 
-# model.summary()
+# print(hist)
 
 # print_all(data)
 # print_all(dataX)
