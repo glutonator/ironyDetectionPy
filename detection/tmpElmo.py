c82b8cbd5b9b4e4dce2c3a840821d70b2c9244ff
Glutek <glutonator@gmail.com>
2019-12-25 21:44:55 +0100

    working elmo example

--- detection/tmpElmo.py
+++ detection/tmpElmo.py
@@ -1,5 +1,22 @@
+import tensorflow.compat.v1 as tf
 import tensorflow_hub as hub
-import tensorflow as tf
+
+# to jest ważne, bez tego nie działa z tf v2.0
+tf.disable_eager_execution()
+
+elmo = hub.Module("https://tfhub.dev/google/elmo/3")
 
 
-elmo = hub.Module("https://tfhub.dev/google/elmo/2", trainable=True)
+def elmo_vectors(x):
+    embeddings = elmo(x, signature="default", as_dict=True)["elmo"]
+
+    with tf.Session() as sess:
+        sess.run(tf.global_variables_initializer())
+        sess.run(tf.tables_initializer())
+        # return average of ELMo features
+        return sess.run(embeddings)
+
+
+x = [':)']
+vector = elmo_vectors(x)
+print(vector)
