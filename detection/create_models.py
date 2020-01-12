f04b5571f605ac56d8312b3ecf74c177632fae2d
Glutek <glutonator@gmail.com>
2020-01-12 20:20:13 +0100

    fix data

--- detection/create_models.py
+++ detection/create_models.py
@@ -8,23 +8,23 @@ import detection.irony_models_cpu as di_cpu
 def get_all_models_gpu(total_length, max_sentence_length):
     models: List[Sequential] = []
 
-    # models.append(di_gpu.give_model_8000_cnn(total_length, max_sentence_length))
-    # models.append(di_gpu.give_model_8001_cnn(total_length, max_sentence_length))
-    # models.append(di_gpu.give_model_8002_cnn(total_length, max_sentence_length))
+    models.append(di_gpu.give_model_8000_cnn(total_length, max_sentence_length))
+    models.append(di_gpu.give_model_8001_cnn(total_length, max_sentence_length))
+    models.append(di_gpu.give_model_8002_cnn(total_length, max_sentence_length))
 
     models.append(di_gpu.give_model_00(total_length, max_sentence_length))
-    # models.append(di_gpu.give_model_10(total_length, max_sentence_length))
-    # models.append(di_gpu.give_model_20(total_length, max_sentence_length))
-    # models.append(di_gpu.give_model_30(total_length, max_sentence_length))
-    # models.append(di_gpu.give_model_40(total_length, max_sentence_length))
-    # models.append(di_gpu.give_model_50(total_length, max_sentence_length))
-    # models.append(di_gpu.give_model_60(total_length, max_sentence_length))
-    #
-    # models.append(di_gpu.give_model_41(total_length, max_sentence_length))
-    # models.append(di_gpu.give_model_61(total_length, max_sentence_length))
-    #
-    # models.append(di_gpu.give_model_50000(total_length, max_sentence_length))
-    # models.append(di_gpu.give_model_50001(total_length, max_sentence_length))
+    models.append(di_gpu.give_model_10(total_length, max_sentence_length))
+    models.append(di_gpu.give_model_20(total_length, max_sentence_length))
+    models.append(di_gpu.give_model_30(total_length, max_sentence_length))
+    models.append(di_gpu.give_model_40(total_length, max_sentence_length))
+    models.append(di_gpu.give_model_50(total_length, max_sentence_length))
+    models.append(di_gpu.give_model_60(total_length, max_sentence_length))
+
+    models.append(di_gpu.give_model_41(total_length, max_sentence_length))
+    models.append(di_gpu.give_model_61(total_length, max_sentence_length)) 
+
+    models.append(di_gpu.give_model_50000(total_length, max_sentence_length))
+    models.append(di_gpu.give_model_50001(total_length, max_sentence_length))
 
     return models
 
